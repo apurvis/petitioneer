a10e2de0ebc89631dc387442348c697ca0d167f2
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-10-22 20:50:48 -0400

    Fix skype link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
     puts "params: #{params}"
     if params[:search] && params[:search][:zip]
       @legislators = SunlightManager.new.get_by_zip(params[:search][:zip])
-      @legislators.each { |l| l['skype_link'] = 'skype:+' + l['phone'].gsub(/[^\d]/, '') + '?call' }
+      @legislators.each { |l| l['skype_link'] = 'skype:+1' + l['phone'].gsub(/[^\d]/, '') + '?call' }
       puts @legislators
     end
   end
