a59ccdf269943604300c5ce3e44b4ea1f2104824
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-10-22 21:03:28 -0400

    Tel: style links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,8 +5,13 @@ class WelcomeController < ApplicationController
     puts "params: #{params}"
     if params[:search] && params[:search][:zip]
       @legislators = SunlightManager.new.get_by_zip(params[:search][:zip])
-      @legislators.each { |l| l['skype_link'] = 'skype:+1' + l['phone'].gsub(/[^\d]/, '') + '?call' }
-      puts @legislators
+      if @legislators.empty?
+        flash.alert = "No representatives found."
+        redirect_to welcome_index_path
+      else
+        @legislators.each { |l| l['skype_link'] = 'skype:+1' + l['phone'].gsub(/[^\d]/, '') + '?call' }
+        puts @legislators
+      end
     end
   end
 end
